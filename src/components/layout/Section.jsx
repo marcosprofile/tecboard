63608795cdcd1b8f832ad42cab75461bfa233eb1
marcosprofile <marcos_laurindo1997@hotmail.com>
2025-06-26 00:45:53 -0300

    feat: update padding left and right from section

--- src/components/layout/Section.jsx
+++ src/components/layout/Section.jsx
@@ -1,6 +1,6 @@
 export default function Section(props) {
   return (
-    <section className={`flex flex-col items-center my-16 px-4 max-w-7xl w-full ${props.classes}`}>
+    <section className={`flex flex-col items-center my-16 px-6 max-w-7xl w-full ${props.classes}`}>
       { props.children }
     </section>
   )
